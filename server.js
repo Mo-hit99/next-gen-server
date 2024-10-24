f79ffe25a4fbc67834f340548949af3806c28d2a
EvolMohit <125653626+EvolMohit@users.noreply.github.com>
2024-10-24 16:03:12 +0530

    add all products

--- server.js
+++ server.js
@@ -14,13 +14,17 @@ dotenv.config();
 
 const port= process.env.PORT || 8080;
 const app = express();
+app.use(helmet({
+  hidePoweredBy: true, // Hides X-Powered-By
+  frameguard: { action: 'deny' }, // Prevents clickjacking (removes X-Frame-Options)
+  xssFilter: true // Adds XSS protection header
+}));
 const corsOptions = {
   origin: 'https://next-gen-clothings.vercel.app', // Allow this origin
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   allowedHeaders: ['Content-Type', 'Authorization'], 
   credentials: true, // If you need to include credentials (cookies, authorization headers)
 };
-app.use(helmet());
 app.use(cors(corsOptions))
 app.use(express.json())
 
