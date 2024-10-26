e13c8569b30b6c5c495eef5f4ba19d05d5e2e002
EvolMohit <125653626+EvolMohit@users.noreply.github.com>
2024-10-26 20:13:08 +0530

    add

--- routes/CustomerCare_router.js
+++ routes/CustomerCare_router.js
@@ -2,11 +2,11 @@ import express from "express";
 import dotenv from 'dotenv'
 import upload from "../Image_Multer/Image_multer.js";
 import { getMessageData, MessageSender } from "../controller/CustomerCareChatBox.js";
-dotenv.config();
 import multer from "multer"
 import {v2 as cloudinary} from 'cloudinary';
 import { CloudinaryStorage }  from 'multer-storage-cloudinary';
 
+dotenv.config();
 
 
 
@@ -32,17 +32,12 @@ const storage = new CloudinaryStorage({
       `${Date.now()}-${file.originalname.split('.')[0]}`
     }
   },
-  folder: 'uploads', // The name of the folder in Cloudinary
 });
 
 // Create multer instance
 
-
 const upload = multer({ storage: storage })
 
 export const CustomerCareChatBox_router = express.Router()
-
-
-
 CustomerCareChatBox_router.get('/api/messages',getMessageData)
-CustomerCareChatBox_router.post('/api/messages',upload.single('image'),MessageSender)
+CustomerCareChatBox_router.post('/api/messages',upload.single('product-img'),MessageSender)
