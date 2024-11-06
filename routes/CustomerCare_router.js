92dc085812a60a842e8ae6fd368dffeabb2f7e36
EvolMohit <125653626+EvolMohit@users.noreply.github.com>
2024-11-06 20:55:21 +0530

    addss

--- routes/CustomerCare_router.js
+++ routes/CustomerCare_router.js
@@ -1,44 +1,11 @@
 import express from "express";
 // import dotenv from 'dotenv'
-import { getMessageData, MessageSender } from "../controller/CustomerCareChatBox.js";
+import { deleteAllMessage, deleteMessage, getMessageData, MessageSender } from "../controller/CustomerCareChatBox.js";
 import { upload } from "../Image_Multer/Image_multer.js";
-// import multer from "multer"
-// import {v2 as cloudinary} from 'cloudinary';
-// import { CloudinaryStorage }  from 'multer-storage-cloudinary';
 
-// dotenv.config();
-
-
-
-// // upload.js
-
-// // Configure Cloudinary
-// cloudinary.config({
-//   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-//   api_key: process.env.CLOUDINARY_API_KEY,
-//   api_secret: process.env.CLOUDINARY_API_SECRET,
-  
-// });
-
-// // Configure multer storage
-// const storage = new CloudinaryStorage({
-//   cloudinary: cloudinary,
-//   params:{
-//     format: async (req,file)=>{
-//       const fileType= file.mimetype.split('/')[1];
-//       return ['jpeg','png','jpg','gif'].includes(fileType) ? fileType : 'jpeg'
-//     },
-//     public_id:(req,file)=>{
-//       `${Date.now()}-${file.originalname.split('.')[0]}`
-//     },
-//     folder: 'uploads-img', // The name of the folder in Cloudinary
-//   },
-// });
-
-// // Create multer instance
-
-// const upload = multer({ storage: storage })
 
 export const CustomerCareChatBox_router = express.Router()
 CustomerCareChatBox_router.get('/api/messages',getMessageData)
 CustomerCareChatBox_router.post('/api/messages',upload.single('product-img'),MessageSender)
+CustomerCareChatBox_router.delete('/api/messages/delete/:id',deleteMessage)
+CustomerCareChatBox_router.delete('api/messages/deleteAll',deleteAllMessage);
