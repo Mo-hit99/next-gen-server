16c3735bfe15b048345723c585c9ef5f9fee981e
EvolMohit <125653626+EvolMohit@users.noreply.github.com>
2024-11-12 19:45:18 +0530

    addss

--- template/template.js
+++ template/template.js
@@ -5,7 +5,7 @@ export let optTemplate = (email,otp)=>{
       <a href="" style="font-size:1.4em;color: #00466a;text-decoration:none;font-weight:600">NextGenClothes</a>
     </div>
     <p style="font-size:1.1em">Hi,${email}</p>
-    <p>Thank you for choosing NextGen. Use the following OTP to complete your Sign Up procedures. OTP is valid for 5 minutes</p>
+    <p>Thank you for choosing NextGenClothes. Use the following OTP to complete your Sign Up procedures. OTP is valid for 5 minutes</p>
     <h2 style="background: #00466a;margin: 0 auto;text-align:center;width:400px;padding: 0 10px;color: #fff;border-radius: 4px;">${otp}</h2>
     <p style="font-size:0.9em;">Regards,<br />NextGenClothes</p>
     <hr style="border:none;border-top:1px solid #eee" />
